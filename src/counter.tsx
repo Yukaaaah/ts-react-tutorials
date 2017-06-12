778f818b14dd513122030c68b12562b104dcb4c5
Yuka Hamano <hamano@solairo.co.jp>
2017-06-12 17:41:46 +0900

    counter.tsx 処理を分ける

--- src/counter.tsx
+++ src/counter.tsx
@@ -3,7 +3,8 @@ import * as ReactDom from "react-dom";
 
 export function renderCounter(){
     ReactDom.render(
-        <Counter state={counterState} onClick={addCounter}/>,
+        <Counter state={counterState}
+                 onClick={handleCounter}/>,
         document.getElementById("content")
     );
 
@@ -15,8 +16,12 @@ interface CounterState {
 
 interface CounterProps {
     state: CounterState
-    onClick: () => void
-//  void: 返り値を持たない関数を表す
+    onClick: (action: string) => void
+}
+
+interface ActionButtonProps {
+    onClick: (action: string) => void
+    actionName: string
 }
 
 let counterState: CounterState = {
@@ -24,19 +29,33 @@ let counterState: CounterState = {
 };
 
 
-function addCounter() {
-    counterState.count += 1;
+function handleCounter(action: string) {
+    if (action === "Add") {
+        counterState.count += 1;
+    }
+    if (action === "Minus"){
+        counterState.count -= 1;
+    }
     renderCounter();
 }
 
+
 function Counter(props: CounterProps) {
-        return <div className="center-c ph3">
-            <a className="f6 link dim ph3 pv2 mb2 dib white bg-purple" onClick={props.onClick}>
-                Count Up!
-            </a>
+        return <div>
+                <ActionButton actionName="Add" onClick={props.onClick}/>
+                <ActionButton actionName="Minus" onClick={props.onClick}/>
             <div className="tc f3 pt4">
                 {props.state.count} times click
             </div>
-        </div>;
+            </div>;
     }
+
+function ActionButton (props: ActionButtonProps) {
+    return <div className="center-c ph3">
+        <a className="f6 link dim ph3 pv2 mb2 dib white bg-purple"
+           onClick={() => props.onClick(props.actionName)}>
+            {props.actionName} button
+        </a>
+    </div>
+}
 
